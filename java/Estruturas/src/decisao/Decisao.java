fd5696218695bbcc0107d8564beb2d413783814b
pfalencar <patricia.alencar@etec.sp.gov.br>
2018-08-30 17:23:34 -0300

    Estruturas

---
+++ java/Estruturas/src/decisao/Decisao.java
@@ -0,0 +1,17 @@
+package decisao;
+
+public class Decisao {
+
+	public static void main(String[] args) {
+		
+		int total = 10;
+		if (total > 10) {
+			System.out.println("O total é maior do que 10.");
+		} else if (total == 10) {
+			System.out.println("O total é igual a 10");
+		} else {
+			System.out.println("O total é menor do que 10");
+		}
+	}
+
+}
